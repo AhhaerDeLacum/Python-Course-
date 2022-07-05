af152fc1264caa84481eac47d8021817097705c3
Vladislav Lovell <vladislav.prosvirkin.2000@mail.ru>
2022-07-05 13:01:06 +0300

    Выполнил

--- lesson_002/06_songs_list.py
+++ lesson_002/06_songs_list.py
@@ -38,4 +38,5 @@ violator_songs_dict = {
 # распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
 #   А другие три песни звучат ХХХ минут
 
-# TODO здесь ваш код
+sum1 = violator_songs_dict['Sweetest Perfection'] + violator_songs_dict['Policy of Truth'] + violator_songs_dict['Blue Dress']
+print(f'А другие три песни {sum1} min')

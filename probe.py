00fadab8df6b20e7734b1eec51b08fcb9b34fc72
Vladislav Lovell <vladislav.prosvirkin.2000@mail.ru>
2022-07-05 18:26:29 +0300

    Числа фибоначи

--- probe.py
+++ probe.py
@@ -1,1 +1,15 @@
 #Числа фибоначи*
+def Fibonacci(f1):
+    f2 = 1
+    print(f1)
+    while f2 < 1000:
+        print(f2)
+        f1, f2 = f2, f1 + f2
+
+Fibonacci(1)
+
+# f1, f2 = 1, 1
+#     print(f1)
+#     while f2 < 1000:
+#         print(f2)
+#         f1, f2 = f2, f1+f2

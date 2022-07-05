b4d630284288eb29c64f5c8c63339adf72730076
Vladislav Lovell <vladislav.prosvirkin.2000@mail.ru>
2022-07-05 12:54:28 +0300

    посчитал сумму 3 песен

--- lesson_002/06_songs_list.py
+++ lesson_002/06_songs_list.py
@@ -20,8 +20,8 @@ violator_songs_list = [
 # Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
 
-# TODO здесь ваш код
-
+sum_ = round(violator_songs_list[3][1] + violator_songs_list[5][1] + violator_songs_list[-1][1], 2)
+print(f'3 songs sound like {sum_} min')
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
     'World in My Eyes': 4.76,

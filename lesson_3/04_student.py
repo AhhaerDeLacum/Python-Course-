e7e8f4258a6713cfbd17890379c26ba918fb4287
Vladislav Lovell <vladislav.prosvirkin.2000@mail.ru>
2022-07-06 13:07:51 +0300

    Прорешал задачку + нашел ошибку через дебаг  и проверил ее правильность в интернете по результату + добавил раунд

--- lesson_3/04_student.py
+++ lesson_3/04_student.py
@@ -10,5 +10,16 @@
 #   Студенту надо попросить ХХХ.ХХ рублей
 
 educational_grant, expenses = 10000, 12000
+total_expenses = expenses
+total_grant = educational_grant
+i = 1
+while i < 10:
+    expenses += expenses * 3 / 100
+    total_expenses += expenses
+    total_grant += educational_grant
 
-
+    i += 1
+print(i)
+result = round((total_grant - total_expenses) * (-1), 2)
+print(f'Студенту надо попросить {result} рублей')
+#37566.551737648784

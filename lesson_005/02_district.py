b99c8484c116310601d87f12147caee1bd5673be
Vladislav Lovell <vladislav.prosvirkin.2000@mail.ru>
2022-07-11 18:25:38 +0300

    Подключил модули из двух папок и вывел сумму списков с функцией .join()

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -5,7 +5,13 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-
+from district.central_street.house1.room1 import folks as f1
+from district.central_street.house1.room2 import folks as f2
+from district.central_street.house2.room1 import folks as f3
+from district.central_street.house2.room2 import folks as f4
+f_sum_list = f1 + f2 + f3 + f4
+f1_str = ', '.join(f_sum_list)
+print(f'В районе живут: {f1_str}')
 
 
 

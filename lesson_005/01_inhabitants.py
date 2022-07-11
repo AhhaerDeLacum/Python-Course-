e98364ebb3dfbe13fb63dabbeb5be726940e17d4
Vladislav Lovell <vladislav.prosvirkin.2000@mail.ru>
2022-07-11 14:01:01 +0300

    Add import modules room1 and room2 and add "print with parameters from room 1 and room2"

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -3,5 +3,8 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-
+from room_2 import folks as folks_name
+from room_1 import folks as folks_name1
+print(f'В комнате 1 живут: {folks_name1}')
+print(f'В комнате 2 живут: {folks_name}')
 

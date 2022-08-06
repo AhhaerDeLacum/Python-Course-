9408662dc7aebcf66e7bd4d026d8a832811c5933
Vladislav Lovell <vladislav.prosvirkin.2000@mail.ru>
2022-08-06 14:07:02 +0300

    Задача для теста

--- lesson_009/3ex.py
+++ lesson_009/3ex.py
@@ -12,7 +12,7 @@
 earnings = 0
 while True:
     n_days = int(input('Введите число дней '))
-    if not 2 <= n_days <= 10**5:
+    if not 2 <= n_days <= 10 ** 5:
         print('Не входит в диапазон допустимых значений 1 входные данные')
         continue
     a_number_of_subs = input('Введите число подписок ').split()
@@ -21,15 +21,12 @@ while True:
         # if number == ' ':
         #     print('///')
         #     del a_number_of_subs[i]
-        if not 1 <= number <= 10**3:
+        if not 1 <= number <= 10 ** 3:
             print('Не входит в диапазон допустимых значений 2 входные данные')
             a_number_of_subs = input('Введите число подписок ').split()
             num_list = list(map(int, a_number_of_subs))
             continue
-        earnings += ((-1)**(i+1-1))*(number)
+        earnings += ((-1) ** (i + 1 - 1)) * number
 
     break
 print(abs(earnings))
-
-
-

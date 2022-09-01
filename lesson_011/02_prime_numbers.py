44be267006bb422b36396ca8bec2133430126b06
Vladislav Lovell <vladislav.prosvirkin.2000@mail.ru>
2022-09-01 14:43:18 +0300

    Две первые мини задачи на генератор и итератор решил

--- lesson_011/02_prime_numbers.py
+++ lesson_011/02_prime_numbers.py
@@ -4,15 +4,15 @@
 # Есть функция генерации списка простых чисел
 
 
-def get_prime_numbers(n):
-    prime_numbers = []
-    for number in range(2, n+1):
-        for prime in prime_numbers:
-            if number % prime == 0:
-                break
-        else:
-            prime_numbers.append(number)
-    return prime_numbers
+# def get_prime_numbers(n):
+#     prime_numbers = []
+#     for number in range(2, n+1):
+#         for prime in prime_numbers:
+#             if number % prime == 0:
+#                 break
+#         else:
+#             prime_numbers.append(number)
+#     return prime_numbers
 
 # Часть 1
 # На основе алгоритма get_prime_numbers создать класс итерируемых обьектов,
@@ -46,46 +46,46 @@ def get_prime_numbers(n):
 prime_number_iterator = PrimeNumbers(n=10)
 for number in prime_number_iterator:
     print(number)"""
-class PrimeNumbers:
-    def __init__(self, n):
-        self.n, self.i, self.number, self.counter = n, 1, [], 0
-
-    def __iter__(self):
-        self.i = 1
-        return self
-
-    # def __next__(self):
-    #     self.i += 1
-    #     if self.i > 0:
-    #         if self.i > self.n:
-    #             raise StopIteration()
-    #         for prime in self.number:
-    #             if self.i % prime == 0:
-    #                 break
-    #         else:
-    #             self.number.append(self.i)
-    #             self.counter += 1
-    #             if self.i is not None:
-    #                 return self.i
-    def get_prime_numbers(self):
-        self.i += 1
-        for prime in self.number:
-            if self.i % prime == 0:
-                return False
-        return True
-
-    def __next__(self):
-        while self.i < self.n:
-            if self.get_prime_numbers():
-                self.number.append(self.i)
-                return self.i
-        else:
-            raise StopIteration()
-
-
-prime_number_iterator = PrimeNumbers(n=10)
-for number in prime_number_iterator:
-    print(number)
+# class PrimeNumbers:
+#     def __init__(self, n):
+#         self.n, self.i, self.number, self.counter = n, 1, [], 0
+#
+#     def __iter__(self):
+#         self.i = 1
+#         return self
+#
+#     # def __next__(self):
+#     #     self.i += 1
+#     #     if self.i > 0:
+#     #         if self.i > self.n:
+#     #             raise StopIteration()
+#     #         for prime in self.number:
+#     #             if self.i % prime == 0:
+#     #                 break
+#     #         else:
+#     #             self.number.append(self.i)
+#     #             self.counter += 1
+#     #             if self.i is not None:
+#     #                 return self.i
+#     def get_prime_numbers(self):
+#         self.i += 1
+#         for prime in self.number:
+#             if self.i % prime == 0:
+#                 return False
+#         return True
+#
+#     def __next__(self):
+#         while self.i < self.n:
+#             if self.get_prime_numbers():
+#                 self.number.append(self.i)
+#                 return self.i
+#         else:
+#             raise StopIteration()
+#
+#
+# prime_number_iterator = PrimeNumbers(n=10)
+# for number in prime_number_iterator:
+#     print(number)
 
 
 # Часть 2
@@ -110,19 +110,19 @@ def primes():
 print(list(itertools.takewhile(lambda x : x <= 10000, primes())))
 """
 
-def prime_numbers_generator(n):
-    prime_numbers = []
-    for number in range(2, n+1):
-        for prime in prime_numbers:
-            if number % prime == 0:
-                break
-        else:
-            prime_numbers.append(number)
-            yield number
-
-
-for number in prime_numbers_generator(n=10000):
-    print(number)
+# def prime_numbers_generator(n):
+#     prime_numbers = []
+#     for number in range(2, n+1):
+#         for prime in prime_numbers:
+#             if number % prime == 0:
+#                 break
+#         else:
+#             prime_numbers.append(number)
+#             yield number
+#
+#
+# for number in prime_numbers_generator(n=10000):
+#     print(number)
 
 
 # Часть 3
@@ -140,3 +140,38 @@ for number in prime_numbers_generator(n=10000):
 # простых счастливых палиндромных чисел и так далее. Придумать не менее 2х способов.
 #
 # Подсказка: возможно, нужно будет добавить параметр в итератор/генератор.
+def numbers_generator(lucky_number):
+    len_ = len(str(lucky_number))
+    str_lucky_number = str(lucky_number)#132 2345
+    if len_ >= 3:
+        if len_ % 2 == 0:#4 6 8
+            if int(str_lucky_number[0]) + int(str_lucky_number[1]) == int(str_lucky_number[-1]) + int(str_lucky_number[-2]):
+                return True
+            else:
+                return False
+        elif len_ % 2 == 1: #1 -1
+            if int(str_lucky_number[0]) == int(str_lucky_number[-1]):
+                return True
+            else:
+                return False
+    else:
+        return False
+
+def prime_numbers_generator(n):
+    prime_numbers = []
+    for number in range(2, n + 1):
+        for prime in prime_numbers:
+            if number % prime == 0:
+                break
+        else:
+            prime_numbers.append(number)
+            #yield number ЕСЛИ ЗАКОМЕНИТЬ ДВЕ НИЖНИЕ СТРОКИ И РАССКОМЕНТИТЬ ЭТУ, ТО ПОЛУЧАТСЯ ВСЕ НАТУРАЛЬНЫЕ ЧИСЛА
+            if numbers_generator(number):
+                yield number
+
+
+for number in prime_numbers_generator(n=10000):
+    print(number, numbers_generator(number))
+
+# for number in numbers_generator(n=100, lucky_number=727):
+#     print(number)

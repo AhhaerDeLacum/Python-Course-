3df30a4264a62ddb6c49439d1e02ab6a68df0b06
Vladislav Lovell <vladislav.prosvirkin.2000@mail.ru>
2022-09-14 17:03:03 +0300

    Выполнил задачу в многопроцессном стиле + добавил time_track из модуля utils.

--- lesson_012/03_volatility_with_processes.py
+++ lesson_012/03_volatility_with_processes.py
@@ -17,50 +17,51 @@
 #       ТИКЕР7, ТИКЕР8, ТИКЕР9, ТИКЕР10, ТИКЕР11, ТИКЕР12
 # Волатильности указывать в порядке убывания. Тикеры с нулевой волатильностью упорядочить по имени.
 #
+import multiprocessing
 import os
 import time
 from collections import defaultdict
 from threading import Thread
+from multiprocessing import Queue, Pipe, Process
+import queue
+from utils import time_track
 
-from lesson_012.python_snippets.utils import time_track
 
+class FilesOpenForCalculatingVolatility(multiprocessing.Process):
 
-class FilesOpenForCalculatingVolatility(Thread):
-
-    def __init__(self, path_name, *args, **kwargs):
+    def __init__(self, path_name, collector, *args, **kwargs):
         super(FilesOpenForCalculatingVolatility, self).__init__(*args, **kwargs)
         self.file_name = None
         self.path_name = os.path.normpath(path_name)
         self.full_file_path = None
         self.max_price = None
         self.min_price = None
-        self.average_price = 0 ######
-        self.volatility = 0 ########
+        self.average_price = 0  ######
+        self.volatility = 0  ########
         self.list_volatility = []
         self.list_zero_volatility = []
+        self.collector = collector
 
     def run(self):
         try:
             self.opendir()
             self.list_volatility.sort()
-            #self.list_zero_volatility.sort()
+            self.collector.put((self.list_volatility, self.list_zero_volatility))
         except Exception as exc:
             print(exc)
 
     def opendir(self):
         if self.path_name:
             for dirpath, dirnames, filenames in os.walk(self.path_name):
-                #print(dirpath, dirnames, filenames) &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+                # print(dirpath, dirnames, filenames) &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
                 for file in filenames:
-                    self.file = file # не уверен в надобности
+                    self.file = file  # не уверен в надобности
                     self.full_file_path = os.path.join(dirpath, self.file)
                     self._open()
 
-
-
     def _open(self):
-        #print('###' * 10)
-        self.ticker_dict = defaultdict(int) ###########
+        # print('###' * 10)
+        self.ticker_dict = defaultdict(int)  ###########
         is_it_first_line = True
         with open(self.full_file_path, mode='r', encoding='utf8') as file:
             for line in file:
@@ -68,14 +69,12 @@ class FilesOpenForCalculatingVolatility(Thread):
                     is_it_first_line = False
                 else:
                     line = line[:-1]
-                    #print(line) #######################
+                    # print(line) #######################
                     self.SECID, TRADETIME, PRICE, QUANTITY = line.split(',')
                     self._identification_min_and_max(PRICE)
-            #print('open _calculating') &&&&&&&&&&&
-            #time.sleep(2) &&&&&&&&&&&&&&&&&&&&&
+            # print('open _calculating') &&&&&&&&&&&
+            # time.sleep(2) &&&&&&&&&&&&&&&&&&&&&
             self._calculating()
-
-
 
     # Например для бумаги №1:
     #   average_price = (12 + 11) / 2 = 11.5
@@ -86,7 +85,7 @@ class FilesOpenForCalculatingVolatility(Thread):
         self.volatility = round(self.volatility, 2)
         # print(self.min_price)
         # print(self.max_price)
-        #print(f'{self.volatility}%') %%%%%%%%%%%%%%%%%%%%%%%%
+        # print(f'{self.volatility}%') %%%%%%%%%%%%%%%%%%%%%%%%
         # time.sleep(2)
         if self.volatility == 0:
             self.list_zero_volatility.append(self.SECID)
@@ -94,7 +93,7 @@ class FilesOpenForCalculatingVolatility(Thread):
             self.list_volatility.append((self.volatility, self.SECID))
         self.max_price = None
         self.min_price = None
-        #print(self.list_volatility) ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        # print(self.list_volatility) ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
         # print(self.min_price)
         # print(self.max_price)
         # print(f'{self.volatility}%')
@@ -108,35 +107,31 @@ class FilesOpenForCalculatingVolatility(Thread):
             self.min_price = PRICE
         elif self.max_price < PRICE:
             self.max_price = PRICE
-        #print(PRICE) #####################
+        # print(PRICE) #####################
 
-    def time_track(func):
-        def surrogate(*args, **kwargs):
-            started_at = time.time()
 
-            result = func(*args, **kwargs)
-
-            ended_at = time.time()
-            elapsed = round(ended_at - started_at, 6)
-            print(f'Функция {func.__name__} работала {elapsed} секунд(ы)', )
-            return result
-
-        return surrogate
-
+@time_track
 def main():
-    calculating_files = FilesOpenForCalculatingVolatility(path_name="trades")
+    collector = multiprocessing.Queue()
+    calculating_files = FilesOpenForCalculatingVolatility(path_name="trades", collector=collector)
     calculating_files.start()
+    while True:
+        try:
+            list_volatility, list_zero_volatility = collector.get(timeout=1)
+        except queue.Empty:
+            if not calculating_files.is_alive():
+                break
     calculating_files.join()
 
-    print('Максимальная волатильность:')
-    for max_v in calculating_files.list_volatility[:-4:-1]:
+    print('Максимальная волатильность:');
+    for max_v in list_volatility[:-4:-1]:
         print(max_v)
-    print('Минимальная волатильность:')
-    for min_v in calculating_files.list_volatility[2::-1]:
+    print('Минимальная волатильность:');
+    for min_v in list_volatility[2::-1]:
         print(min_v)
     print('Нулевая волатильность:')
 
-    result = [x for x in calculating_files.list_zero_volatility]
+    result = [x for x in list_zero_volatility]
     print(result)
 
 
